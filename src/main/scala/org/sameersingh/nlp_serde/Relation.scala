93eb7291141ae8b122f3dec6a150b0f3d530335f
Sameer Singh <sameeersingh@gmail.com>
2014-11-04 12:28:15 -0800

    add support for relation types

--- src/main/scala/org/sameersingh/nlp_serde/Relation.scala
+++ src/main/scala/org/sameersingh/nlp_serde/Relation.scala
@@ -23,3 +23,39 @@ class Relation extends Attr {
 
   def toCase = immutable.Relation(m1Id, m2Id, relations.toSet, attrs.toMap)
 }
+
+trait RelationType {
+  // string that represents the complete relation type
+  def string: String
+
+  // human readable
+  def pretty(arg1: String = "X", arg2: String = "Y"): String
+}
+
+trait Pattern extends RelationType {
+  def name: String
+
+  // string that represents the complete relation type
+  override def string: String = "Pattern\tname"
+
+  // human readable
+  override def pretty(arg1: String = "X", arg2: String = "Y") = "%s %s %s" format(arg1, name, arg2)
+}
+
+trait FreebaseRelation extends RelationType {
+  def mid: String
+
+  def name: String
+
+  // true if arg2 comes before arg1
+  def reverse: Boolean
+
+  // string that represents the complete relation type
+  override def string: String = s"Freebase\t$name\t$mid\t$reverse"
+
+  // human readable
+  override def pretty(arg1: String = "X", arg2: String = "Y") = {
+    val ord = if (reverse) (arg2, arg1) else (arg1, arg2)
+    "%s is %s of %s" format(ord._1, name, ord._2)
+  }
+}
