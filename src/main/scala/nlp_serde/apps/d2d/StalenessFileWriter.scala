4470f2fe0dd21edc99adf9e565719f0236a917f9
Sameer Singh <sameeersingh@gmail.com>
2014-11-09 02:21:31 -0800

    minor refactoring in staleness

--- src/main/scala/nlp_serde/apps/d2d/StalenessFileWriter.scala
+++ src/main/scala/nlp_serde/apps/d2d/StalenessFileWriter.scala
@@ -4,7 +4,7 @@ import java.io.PrintWriter
 import java.text.SimpleDateFormat
 import java.util.Date
 
-import nlp_serde.Document
+import nlp_serde.{Entity, Document}
 import nlp_serde.readers.PerLineJsonReader
 import nlp_serde.writers.{HTMLWriter, Writer}
 
@@ -14,7 +14,7 @@ import nlp_serde.writers.{HTMLWriter, Writer}
 class StalenessFileWriter extends Writer {
   override def write(name: String, docs: Iterator[Document]): Unit = {
     val writer = new PrintWriter(name)
-    for(d <- docs) {
+    for (d <- docs) {
       write(writer, d)
     }
     writer.flush()
@@ -23,27 +23,30 @@ class StalenessFileWriter extends Writer {
 
   case class Entry(id: String, toks: Set[String], date: Date)
 
+  def entityId(e: Entity): Option[String] = if (e.ner.get != "O" && e.freebaseIds.size > 0) Some(e.freebaseIds.maxBy(_._2)._1) else None
+
   def entityEntries(d: Document): Seq[Entry] = {
-    if(!d.attrs.contains("date")) return Seq.empty
+    if (!d.attrs.contains("date")) return Seq.empty
     val f = new SimpleDateFormat("yyyy-MM-dd")
     val date = f.parse(d.attrs("date"))
-    d.entities.filter(_.ner.isDefined).map(e => {
-      val id = if(e.freebaseIds.size > 0) e.freebaseIds.maxBy(_._2)._1 else e.representativeString
-      val toks = d.sentences.flatMap(s => {
-        s.mentions.filter(e.mids contains _.id).flatMap(m => {
-          s.tokens.filter(t => {
-            val pos = t.pos.getOrElse("O")
-            pos.startsWith("N") || pos.startsWith("V")
-          }).map(t => t.lemma.getOrElse(t.text))
-        })
-      }).toSet
-      Entry(id, toks, date)
+    d.entities.filter(_.ner.isDefined).flatMap(e => {
+      entityId(e).map(id => {
+        val toks = d.sentences.flatMap(s => {
+          s.mentions.filter(e.mids contains _.id).flatMap(m => {
+            s.tokens.filter(t => {
+              val pos = t.pos.getOrElse("O")
+              (pos.startsWith("N") || pos.startsWith("V")) && !pos.startsWith("NNP")
+            }).map(t => t.lemma.getOrElse(t.text))
+          })
+        }).toSet
+        Entry(id, toks, date)
+      }).toSeq
     })
   }
 
   def write(writer: PrintWriter, d: Document): Unit = {
     val entries = entityEntries(d)
-    for(e <- entries) {
+    for (e <- entries) {
       writer.println("%s\t%s\t%s\t%d" format(d.id, e.id, e.toks.map(_.trim).mkString("|"), e.date.getTime))
     }
   }
@@ -52,7 +55,7 @@ class StalenessFileWriter extends Writer {
 object StalenessFileWriter {
   def main(args: Array[String]): Unit = {
     val baseDir = "/Users/sameer/Google Drive/UW/D2D/D2D Nov 14/" //"/home/sameer/data/d2d/demo2015/nov/"
-    val inputFile = baseDir + "nigeria_dataset_v04.nlp.json.gz"
+    val inputFile = baseDir + "nigeria_dataset_v04.nlp.lr.json.gz"
     val outputFile = baseDir + "nigeria_dataset_v04.staleness.txt"
     val reader = new PerLineJsonReader()
     val docs = reader.read(inputFile)
