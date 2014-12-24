484e7c0ae45b98b21cf486f10a605abe2d9a89d3
Sameer Singh <sameeersingh@gmail.com>
2014-12-23 22:45:23 -0800

    working nyt reader

--- src/main/scala/nlp_serde/readers/NYTReader.scala
+++ src/main/scala/nlp_serde/readers/NYTReader.scala
@@ -6,36 +6,48 @@ import java.text.SimpleDateFormat
 import com.nytlabs.corpus.{NYTCorpusDocument, NYTCorpusDocumentParser}
 import nlp_serde.Document
 import nlp_serde.writers.PerLineJsonWriter
+import scala.collection.JavaConversions._
 
 /**
  * @author sameer
  * @since 12/23/14.
  */
-class NYTReader(val filter: NYTCorpusDocument => Boolean = x => true, val validating: Boolean = true) extends DocPerFile {
+class NYTReader(val filter: NYTCorpusDocument => Boolean = x => true, val validating: Boolean = false) extends DocPerFile {
   val parser = new NYTCorpusDocumentParser()
-
+  val classes = new collection.mutable.HashMap[String, Int]
   override def readDoc(name: String): Option[Document] = {
-    if(!name.endsWith(".txt")) return None
+    if(!name.endsWith(".xml")) return None
     val f = new File(name)
     val nytDoc = parser.parseNYTCorpusDocumentFromFile(f, validating)
     if(!filter(nytDoc)) return None
     val doc = new Document()
     doc.id = nytDoc.getGuid.toString
-    println(nytDoc.toString)
-    System.exit(1)
-    doc.text
+    doc.path = Some(nytDoc.getSourceFile.getAbsolutePath.replaceAll(".*/data/", ""))
+    //System.exit(1)
+    doc.text = nytDoc.getBody
     val dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
     doc.attrs("date") = dateFormatter.format(nytDoc.getPublicationDate)
-    None
+    doc.attrs("taxonomicClassifiers") = nytDoc.getTaxonomicClassifiers.mkString("\t")
+    doc.attrs("title") = nytDoc.getHeadline
+    doc.attrs("onlineSection") = nytDoc.getOnlineSection
+    doc.attrs("newsDesk") = nytDoc.getNewsDesk
+    doc.attrs("typesOfMaterial") = nytDoc.getTypesOfMaterial.mkString("\t")
+    doc.attrs("url") = nytDoc.getUrl.toString
+    for(c <- nytDoc.getTaxonomicClassifiers) {
+      classes(c) = 1 + classes.getOrElse(c, 0)
+    }
+    Some(doc)
   }
 }
 
 object NYTReader {
   def main(args: Array[String]): Unit = {
     val nytBase = "/home/sameer/work/data/nyt/nyt/"
-    val reader = new NYTReader()
-    val docs = reader.readFilelist(nytBase + "docs/file.tbl", l => nytBase + "data" + l.drop(3))
+    //val nytBase = "/Users/sameer/Work/data/nyt/nyt/"
+    val reader = new NYTReader(_.getTaxonomicClassifiers.forall(!_.contains("Top/Classifieds")))
+    val docs = reader.readFilelist(nytBase + "docs/files.tbl", l => nytBase + "data" + l.drop(3))
     val writer = new PerLineJsonWriter(true)
     writer.write(nytBase + "../nyt.txt.json.gz", docs)
+    println(reader.classes.toSeq.sortBy(_._2).map(p => p._1 + "\t" + p._2).mkString("\n"))
   }
 }
