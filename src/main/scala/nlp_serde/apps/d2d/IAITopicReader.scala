b61c0370349d95ccd5028614120452bb973eb41e
Sameer Singh <sameeersingh@gmail.com>
2015-02-01 22:43:40 -0800

    d2d: update topic reader, add launcher

--- src/main/scala/nlp_serde/apps/d2d/IAITopicReader.scala
+++ src/main/scala/nlp_serde/apps/d2d/IAITopicReader.scala
@@ -1,10 +1,14 @@
 package nlp_serde.apps.d2d
 
+import java.io.PrintWriter
+
 import com.typesafe.scalalogging.slf4j.Logging
 import nlp_serde.Document
 import nlp_serde.readers.PerLineJsonReader
+import nlp_serde.writers.PerLineJsonWriter
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Created by sameer on 1/30/15.
@@ -19,7 +23,7 @@ class IAITopicReader(dir: String = "data/d2d/iai/") extends Logging {
   def readTopics(model: String = "content"): Unit = {
     logger.info(s"Reading $model topics")
     val source = io.Source.fromFile(dir + model + "/temp_allafrica-topic-single.txt")
-    for(l <- source.getLines()) {
+    for (l <- source.getLines()) {
       logger.trace(s"line: {$l}")
       val split = l.split("\t")
       assert(split.length == 2)
@@ -27,7 +31,7 @@ class IAITopicReader(dir: String = "data/d2d/iai/") extends Logging {
       logger.trace(s"split1: {${split(1)}}")
       val ids = split(0).split("\\/")
       val id = ids(0)
-      if(id.isEmpty){
+      if (id.isEmpty) {
         logger.warn("Empty Id!: " + l)
       } else {
         val date = if (ids.length != 2) {
@@ -50,7 +54,7 @@ class IAITopicReader(dir: String = "data/d2d/iai/") extends Logging {
   def readIAIIds(): Unit = {
     logger.info(s"Reading allafrica ids")
     val source = io.Source.fromFile(dir + "/allafrica_output.tsv", "ISO-8859-1")
-    for(l <- source.getLines().drop(1)) {
+    for (l <- source.getLines().drop(1)) {
       val split = l.split("\t").map(_.replaceAll("\"", ""))
       assert(split.length == 12)
       val id = split(0)
@@ -60,9 +64,9 @@ class IAITopicReader(dir: String = "data/d2d/iai/") extends Logging {
       val site = split(4)
       val url = split(5).replaceAll("http://allafrica.com/stories/", "").replaceAll(".html", "")
       val date = split(6)
-      if(!url.isEmpty) {
+      if (!url.isEmpty) {
         idToTitle(id) = title
-        if(urlToId.contains(url))
+        if (urlToId.contains(url))
           logger.warn(s"$url already exists with ${urlToId(url)}, can't add $id")
         urlToId(url) = id
       }
@@ -72,14 +76,14 @@ class IAITopicReader(dir: String = "data/d2d/iai/") extends Logging {
   }
 
   def getTopic(d: Document): Option[Int] = {
-    if(d.path.get.contains("allafrica.com")) {
+    if (d.path.get.contains("allafrica.com")) {
       val url = d.path.get.replaceAll(".*stories/", "").replaceAll("\\.txt", "").take(12)
       val iaiID = urlToId.get(url)
       // println(d.path.get + "("+url+")")
       // println(iaiID)
-      if(iaiID.isEmpty) logger.warn("cannot find id in " + d.path.get)
+      if (iaiID.isEmpty) logger.warn("cannot find id in " + d.path.get)
       val topic = docTopics.get(iaiID.get)
-      if(topic.isEmpty) logger.warn("cannot find topic for " + iaiID.get)
+      if (topic.isEmpty) logger.warn("cannot find topic for " + iaiID.get)
       topic
     } else None
   }
@@ -89,12 +93,24 @@ class IAITopicReader(dir: String = "data/d2d/iai/") extends Logging {
 object IAITopicReader {
   def main(args: Array[String]): Unit = {
     val input = args(0)
-    val topic = args(1).toInt
     val topics = new IAITopicReader()
-    topics.readTopics()
+    val typ = "content"
+    topics.readTopics(typ)
     topics.readIAIIds()
-    for(doc <- new PerLineJsonReader().read(input)) {
-      topics.getTopic(doc)
+    val docs = new mutable.HashMap[Int, ArrayBuffer[Document]]()
+    val writer = new PrintWriter("data/d2d/iai/" + typ + "/id_topics.tsv")
+    for (doc <- new PerLineJsonReader().read(input)) {
+      for (topic <- topics.getTopic(doc)) {
+        writer.println(doc.id + "\t" + topic)
+        docs.getOrElseUpdate(topic, ArrayBuffer.empty) += doc
+      }
+    }
+    writer.flush()
+    writer.close()
+    println("Writing documents")
+    for ((t, ds) <- docs) {
+      val w = new PerLineJsonWriter()
+      w.write("data/d2d/iai/" + typ + "/topic-" + t + ".json.gz", ds.iterator)
     }
   }
 }
